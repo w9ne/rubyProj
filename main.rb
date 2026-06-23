6eaa1cbd0ba98c26ab966f7d1112e8971a605e0d
w9ne <jared119@gmail.com>
2026-06-22 23:13:00 -1000

    AI: Add subclass feature to display retro vs modern

--- main.rb
+++ main.rb
@@ -23,11 +23,44 @@ class Game
 
     def gameStatus
         status = @available ? "Available" : "Unavailable"
-        puts <<~EOF
+        <<~EOF
             #{@title} released in the year #{@year}, with a rating of #{@rating}
             (#{status})
         EOF
     end
+end
+
+#subclass to game (a seperation of retro and modern)
+class RetroGame < Game
+  attr_reader :platform
+
+  # grabbing "game" class title year and rating with the addition of platform
+  def initialize(title, year, rating, platform)
+    super(title, year, rating)
+    @platform = platform
+  end
+
+  #Borrows game status from parent class, with addition of platform string
+  def gameStatus
+    base = super
+    base + "  [Retro - Platform: #{@platform}]\n"
+  end
+end
+
+# modern game is similar
+class ModernGame < Game
+  attr_accessor :has_dlc
+
+  def initialize(title, year, rating, has_dlc)
+    super(title, year, rating)
+    @has_dlc = has_dlc
+  end
+
+  def gameStatus
+    base = super
+    dlc_text = @has_dlc ? "Has DLC" : "No DLC"
+    base + "  [Modern - #{dlc_text}]\n"
+  end
 end
 
 # Actual game lib
@@ -63,8 +96,13 @@ end
 
 # test area
 gameLib = GameLibrary.new
-gameLib.addGame(Game.new("Mario", "1980", "9/10"))
-gameLib.addGame(Game.new("Metal Gear", "2001", "9.5/10"))
-gameLib.addGame(Game.new("Ace Combat Zero", "2006", "10/10"))
+
+#retro
+gameLib.addGame(RetroGame.new("Mario", "1980", "9/10", "NES"))
+gameLib.addGame(RetroGame.new("Ace Combat Zero", "2006", "10/10", "PS2"))
+
+#modern
+gameLib.addGame(ModernGame.new("Forza Horizon 6", "2026", "7/10", false))
+gameLib.addGame(ModernGame.new("Sekiro: Shadows Die Twice", "2019", "10/10", false))
 
 gameLib.list_games #lists game statuses
